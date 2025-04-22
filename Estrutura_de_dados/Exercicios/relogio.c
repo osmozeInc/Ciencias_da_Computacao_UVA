1e783e09356c2f3f3723d647dc3b4aa0812cfc04
Caio Monte <168784455+osmozeInc@users.noreply.github.com>
2025-04-22 15:58:26 -0300

    Create relogio.c

---
+++ Estrutura_de_dados/Exercicios/relogio.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void AtualizarTempo(int *h, int *m, int *s, short *fim);
+void MostrarTempo(int h, int m, int s);
+
+
+int main(){
+    int horas = 0;
+    int minutos = 0;
+    int segundos = 0;
+    short fim = 0;
+
+    while(fim <= 1){
+    MostrarTempo(horas, minutos, segundos);
+    AtualizarTempo(&horas, &minutos, &segundos, &fim);
+    sleep(1);
+    }
+
+    return 0;
+}
+
+
+void AtualizarTempo(int *h, int *m, int *s, short *fim){
+
+    if (*h == 0 && *m == 0 && *s == 0){
+    *h = 23;
+    *m = 59;
+    *s = 60;
+
+    *fim += 1;
+    }
+
+    else if (*m == 0 && *s == 0){
+    *m = 59;
+    *s = 60;
+    *h -= 1;
+    }
+
+    else if (*s == 0){
+    *s = 60;
+    *m -= 1;
+    }
+
+    *s -= 1;
+}
+
+void MostrarTempo(int h, int m, int s){
+    system("clear");
+    printf("%02d:%02d:%02d\n", h, m, s);
+}
