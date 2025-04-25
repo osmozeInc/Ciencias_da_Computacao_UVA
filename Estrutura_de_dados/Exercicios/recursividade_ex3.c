5d88d60c0072ac05fe53e743c1b5e1f6f5de1744
Caio Monte <caiomm076@gmail.com>
2025-04-25 13:09:04 -0300

    exercicio de estrutura

--- Estrutura_de_dados/Exercicios/recursividade_ex3.c
+++ Estrutura_de_dados/Exercicios/recursividade_ex3.c
@@ -1,5 +1,7 @@
-
 #include <stdio.h>
+#include <stdlib.h>
+
+
 void Procurar8PelaMatriz(int matriz[4][4], int linha, int coluna);
 void DesenharMatriz(int matriz[4][4], int linha, int coluna);
 
@@ -8,55 +10,65 @@ int main(){
     int linha = 0;
     int coluna = 0;
 
-    DesenharMatriz(matriz, linha, coluna);
-    //Procurar8PelaMatriz(matriz, linha, coluna);
+    Procurar8PelaMatriz(matriz, linha, coluna);
 
     return 0;
 }
 
 void Procurar8PelaMatriz(int matriz[4][4], int linha, int coluna){
-    printf("\nPosição do cursor é: [%d][%d]\n"
-            "Numero nessa posicao eh: %d,", linha, coluna, matriz[linha][coluna]);
+    system("cls ||clear");
+    DesenharMatriz(matriz, linha, coluna);
+
+    printf("\nPosicao do cursor e: [%d][%d]\n"
+            "Numero nessa posicao eh: %d, ", linha + 1, coluna + 1, matriz[linha][coluna]);
 
     switch (matriz[linha][coluna]){
         case 1:
             printf("indo para cima\n");
             linha--;
+            printf("pressione enter para continuar\n");
+            getchar();
             Procurar8PelaMatriz(matriz, linha, coluna);
             break;
         case 2:
             printf("indo para direita\n");
             coluna++;
+            printf("pressione enter para continuar\n");
+            getchar();
             Procurar8PelaMatriz(matriz, linha, coluna);
             break;
         case 3:
             printf("indo para baixo\n");
             linha++;
+            printf("pressione enter para continuar\n");
+            getchar();
             Procurar8PelaMatriz(matriz, linha, coluna);
             break;
         case 4:
             printf("indo para esquerda\n");
             coluna--;
+            printf("pressione enter para continuar\n");
+            getchar();
             Procurar8PelaMatriz(matriz, linha, coluna);
             break;
         case 8:
-            printf("----------------------------------------\n"
-                    "numero 8 encontrado na posicao: [%d][%d]", linha, coluna);
+            system("cls || clear");
+            DesenharMatriz(matriz, linha, coluna);
+            printf("\n\n----------------------------------------\n"
+                    "numero 8 encontrado na posicao: [%d][%d]", linha + 1, coluna + 1);
             break;
     }
 }
 
 void DesenharMatriz(int matriz[4][4], int linha, int coluna){
-
-    printf("\n-----------------\n|");
-
     for (int l = 0; l < 4; l++){
+        printf("\n-----------------\n|");
         for (int c = 0; c < 4; c++){
         if (c == coluna && l == linha)
-            printf(" X |");
+            printf("\033[33m X \033[0m|");
         else
             printf(" %d |", matriz[l][c]);
         }
-    printf("\n-----------------\n|");
     }
+    printf("\n-----------------\n");
 }
