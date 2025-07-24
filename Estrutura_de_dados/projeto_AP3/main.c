f24e7026c388240a11ff33ffc7215406fa3e269b
Caio Monte <caiomm076@gmail.com>
2025-07-24 14:13:35 -0300

    criar entrada

---
+++ Estrutura_de_dados/projeto_AP3/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+int main() {
+    system("chcp 65001");
+    system("cls");
+    system("gcc animacao.c -o animacao.exe");
+    system("animacao.exe");
+
+    
+    return 0;
+}
